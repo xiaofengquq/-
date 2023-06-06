71aa4f34377fb049e2e27bc4bc746be2d48752ea
yanzhiwei <yanzhiwei@pwrd.com>
2023-06-06 19:48:29 +0800

    上传学习项目

---
+++ src/test/java/Package24/ListTest.java
@@ -0,0 +1,18 @@
+package Package24;
+
+import java.util.List;
+
+/*
+    List接口：
+        1、List集合存储元素特点：有序可重复
+            有序：List集合中的元素有下标
+                从0开始，以1递增
+            可重复：存储一个1，还能存储第二个1
+        2、List既然是Collection接口的子接口，那么肯定List接口有自己“特色”的方法：
+            void add(int index, E element);
+            E get(int index);
+ */
+public class ListTest {
+    public static void main(String[] args) {
+    }
+}

92b4a48251915715fc709ebd7df77005b5d75077
yanzhiwei <yanzhiwei@pwrd.com>
2023-09-08 19:12:39 +0800

    上传学习项目

--- src/test/java/Homework/DirectoryCopy/DirectoryCopy.java
+++ src/test/java/Homework/DirectoryCopy/DirectoryCopy.java
@@ -2,6 +2,8 @@ package Homework.DirectoryCopy;
 
 import java.io.File;
 
+import static Homework.DirectoryCopy.isEmptyDirectory.*;
+
 /*
     要求：
         将整个目录及下面的内容全部拷贝到另一个目录下
@@ -15,9 +17,9 @@ public class DirectoryCopy {
     /**
      * 递归打印源目录下的所有子目录和文件，并执行拷贝操作
      *
-     * @param srcPath   源目录的路径
+     * @param srcPath     源目录的路径
      * @param currentPath 当前处理的目录的路径
-     * @param desPath    目标目录的路径
+     * @param desPath     目标目录的路径
      */
     public static void directoryCopy(String srcPath, String currentPath, String desPath) {
         File currentFile = new File(currentPath);
@@ -27,10 +29,12 @@ public class DirectoryCopy {
             File[] files = currentFile.listFiles();
             if (files != null) {
                 for (File file : files) {
-                    if (file.isDirectory()) {
+                    if (file.isDirectory() && isEmptyDirectory(file) == notEmpty) {
                         System.out.println("目录：" + file);
                         // 递归调用以处理子目录
                         directoryCopy(srcPath, file.getAbsolutePath(), desPath);
+                    } else if (isEmptyDirectory(file) == isEmpty) {
+                        System.out.println("空文件夹" + file.getAbsolutePath());
                     } else {
                         // 处理文件拷贝
                         File resultFile = new File(desPath, getMidPath(srcPath, file.getAbsolutePath()));
@@ -67,5 +71,19 @@ public class DirectoryCopy {
         } else {
             throw new DirectoryCopyException("子路径不包含父路径");
         }
+    }
+
+    public static isEmptyDirectory isEmptyDirectory(File directory) {
+        if (directory.isDirectory()) {
+            String[] filesAndDirs = directory.list();
+            if (filesAndDirs != null && filesAndDirs.length == 0) {
+                //  文件夹为空，返回枚举isEmpty
+                return isEmpty;
+            } else {
+                //  文件夹不为空，返回枚举notEmpty
+                return notEmpty;
+            }
+        }
+        return isFile; // 如果不是目录，返回枚举isFile
     }
 }

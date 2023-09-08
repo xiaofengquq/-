b620dfe2bfb1c3eaf94b4afde8388ebd358dc2a2
yanzhiwei <yanzhiwei@pwrd.com>
2023-09-08 19:13:01 +0800

    上传学习项目

--- src/test/java/Homework/DirectoryCopy/isEmptyDirectory.java
+++ src/test/java/Homework/DirectoryCopy/isEmptyDirectory.java
@@ -4,9 +4,7 @@ public enum isEmptyDirectory {
     isEmpty("目录为空"),
     notEmpty("目录不为空"),
     isFile("不是个目录");
-    private final String name;
 
     isEmptyDirectory(String name) {
-        this.name = name;
     }
 }
